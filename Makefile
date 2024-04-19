e431c1cd4150d600d8bdb76418e7e83cb6b8a29a
Nicholas Grundl <nicholasgrundl@gmail.com>
2024-04-19 11:27:28 -0700

    feat: Initial packaging

---
+++ Makefile
@@ -0,0 +1,69 @@
+#### Python Environment ####
+.PHONY: install
+install: 
+	pip install -r ./build-requirements.txt
+	pipconf --set personal-config
+	pip install -r ./requirements.txt
+	pip install -r ./dev-requirements.txt
+
+.PHONY: uninstall
+uninstall:
+	pipconf --set personal-config
+	@bash -c "pip uninstall -y -r <(pip freeze)"
+
+#### Testing ####
+TEST_COMMANDS = \
+	--cov=tea \
+	--cov-config=setup.cfg \
+	--cov-report html \
+	--cov-report term
+
+.PHONY: test
+test: test.clean test.unit 
+	rm -rf htmlcov/
+	rm -f .coverage
+
+.PHONY: test.clean
+test.clean:
+	rm -rf htmlcov/
+	rm -f .coverage
+
+.PHONY: test.unit
+test.unit: 
+	pytest -rfEP $(TEST_COMMANDS) test/unit
+
+#### Code Style ####
+LINT_DIRS = src/ test/
+LINT_COMMANDS = \
+	isort -q --profile black --check-only $(LINT_DIRS) && \
+	black -q --check --diff $(LINT_DIRS) && \
+	flake8 $(LINT_DIRS) && \
+# eventually use mypy --> mypy --show-error-codes $(LINT_DIRS)
+
+.PHONY: lint
+lint:
+	$(LINT_COMMANDS)
+
+.PHONY: format
+format:
+	isort -q --profile black $(LINT_DIRS)
+	black -q $(LINT_DIRS)
+
+
+
+#### Build ####
+VERSION=`grep version setup.cfg | awk '{print $$3}'`
+TAGNAME=v$(VERSION)
+
+.PHONY: publish.tag
+publish.tag:
+	@echo "---Tagging commit hash $(TAGNAME) "
+	git tag -a $(TAGNAME) -m"Release $(TAGNAME)"
+	git push origin $(TAGNAME)
+	@echo "---Pushed tag as version=$(VERSION)"
+
+
+#### Development ####
+.PHONY: jupyter
+jupyter: 
+	@jupyter lab --autoreload
---
+++ setup.cfg.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+VERSION=`PYTHONPATH=src python -c "import dockmaster; print('.'.join(map(str,dockmaster.__version__)))"`
+AUTHOR=`PYTHONPATH=src python -c "import dockmaster; print(dockmaster.__author__)"`
+AUTHOR_EMAIL=`PYTHONPATH=src python -c "import dockmaster; print(dockmaster.__author_email__)"`
+MAINTAINER=`PYTHONPATH=src python -c "import dockmaster; print(dockmaster.__maintainer__)"`
+MAINTAINER_EMAIL=`PYTHONPATH=src python -c "import dockmaster; print(dockmaster.__maintainer_email__)"`
+DESCRIPTION=`PYTHONPATH=src python -c "import dockmaster; print(dockmaster.__doc__)"`
+REQUIRES=`PYTHONPATH=src python -c "list(map(print,['\t'+line.strip() for line in open('requirements.txt', 'r').readlines()]))"`
+cat <<EOF > setup.cfg
+[metadata]
+name = dockmaster
+version = ${VERSION}
+author = ${AUTHOR}
+author_email = ${AUTHOR_EMAIL}
+maintainer = ${MAINTAINER}
+maintainer_email = ${MAINTAINER_EMAIL}
+description = ${DESCRIPTION}
+license = Apache License 2.0
+url = https://github.com/NicholasGrundl/dockmaster
+
+[options]
+packages_dir = 
+	=src
+packages = find:
+include_package_data = True
+install_requires =
+${REQUIRES}
+
+[options.package_data]
+* = *.json, *.yaml, *.csv
+
+[options.packages.find]
+where=src
+
+[flake8]
+max-line-length = 100
+extend-ignore = E203,F841
+
+[flake8]
+max-line-length = 100
+
+[isort]
+multi_line_output = 3
+force_single_line = true
+include_trailing_comma = true
+force_grid_wrap = 0
+use_parentheses = true
+line_length = 100
+
+[coverage:run]
+branch = True
+EOF
